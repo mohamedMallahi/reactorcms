0557f28700e24f8108fef234b7c1051a63e91540
mohamedMallahi <kingcoding17@gmail.com>
2022-08-18 12:43:37 +0100

    1 file modified, 2 added, 1 deleted

--- pages/admin/delete/[id].js
+++ pages/admin/delete/[id].js
@@ -1,16 +1,24 @@
-import { useState } from 'react';
 import Link from 'next/link';
 import { server } from '../../../config';
 
 export default function Delete() {
+  const deleteHandler = async (e) => {
+    const res = await fetch('/api/todos/1', {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    console.log(data);
+    alert(data.message);
+  };
+
   return (
     <>
       <h1>Admin</h1>
       <h2>Delete Todo</h2>
       <p>Are you sure you want delete this todo</p>
-      <form>
-        <button>Yes</button>
-        <button>No</button>
+      <form onSubmit={deleteHandler}>
+        <button type="submit">Yes</button>
+        <Link href="/admin">No</Link>
       </form>
     </>
   );
