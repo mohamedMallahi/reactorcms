8b3f76a78cad080582ee3b39c0cd01c8a67266e1
mohamedMallahi <kingcoding17@gmail.com>
2022-08-18 12:11:54 +0100

    1 file added

---
+++ pages/admin/index.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Head from 'next/head';
+import { server } from '../config';
+
+export default function Home({ todos }) {
+  return (
+    <>
+      <h1>Admin</h1>
+      <h2>All Todos</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>ID</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr className={todo.completed && 'completed'} key={todo.id}>
+              <td>{todo.title}</td>
+              <td>{todo.id}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch(server + '/api/todos');
+  const data = await res.json();
+  return {
+    props: {
+      todos: data.todos,
+    },
+  };
+}
