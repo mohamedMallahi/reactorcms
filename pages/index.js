b174f208bc4ebafcda93c03dcace6d819157f66c
mohamedMallahi <kingcoding17@gmail.com>
2022-08-18 11:42:46 +0100

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -20,18 +20,11 @@ export default function Home({ todos, err }) {
 }
 
 export async function getServerSideProps(context) {
-  try {
-    const res = await fetch('/api/todos');
-    const data = await res.json();
-    return {
-      props: {
-        todos: data.todos,
-      },
-    };
-  } catch (err) {
-    console.log(err);
-    return {
-      props: { err },
-    };
-  }
+  const res = await fetch('/api/todos');
+  const data = await res.json();
+  return {
+    props: {
+      todos: data.todos,
+    },
+  };
 }
